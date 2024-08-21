ea415ca2cea61ee4ef407b2f0d2fcafbd77e20dc
danmlarsen <larsendanmarius@gmail.com>
2024-08-21 17:17:35 +0200

    Styling & Mobile nav

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
+const bodyEl = document.querySelector('body');
 const openMenuBtnEl = document.getElementById('open-menu-btn');
 const closeMenuBtnEl = document.getElementById('close-menu-btn');
 const navigationMenuEl = document.querySelector('.navigation');
 
-openMenuBtnEl.addEventListener('click', e => {
-    e.preventDefault();
-
+const openNav = function () {
     navigationMenuEl.classList.add('navigation--open');
-});
 
-closeMenuBtnEl.addEventListener('click', e => {
-    e.preventDefault();
+    const backdropEl = document.createElement('div');
+    backdropEl.classList.add('backdrop');
+    backdropEl.addEventListener('click', closeNav);
+    bodyEl.appendChild(backdropEl);
+};
 
+const closeNav = function () {
+    document.querySelector('.backdrop').remove();
     navigationMenuEl.classList.remove('navigation--open');
-});
+};
+
+openMenuBtnEl.addEventListener('click', openNav);
+closeMenuBtnEl.addEventListener('click', closeNav);
