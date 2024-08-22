82c3bb91943711988dac382f3baccf9de11458aa
danmlarsen <larsendanmarius@gmail.com>
2024-08-22 10:44:00 +0200

    Backdrop and focus should work now

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
-const bodyEl = document.querySelector('body');
 const openMenuBtnEl = document.getElementById('open-menu-btn');
 const closeMenuBtnEl = document.getElementById('close-menu-btn');
-const navigationMenuEl = document.querySelector('.navigation');
+const navigationMenuEl = document.getElementById('navigation');
+const backdropEl = document.getElementById('backdrop');
 
-const openNav = function () {
+const openNav = function (e) {
+    e.preventDefault();
+
     navigationMenuEl.classList.add('navigation--open');
-
-    const backdropEl = document.createElement('div');
-    backdropEl.classList.add('backdrop');
-    backdropEl.addEventListener('click', closeNav);
-    bodyEl.appendChild(backdropEl);
+    backdropEl.classList.add('backdrop--show');
 };
 
 const closeNav = function () {
-    document.querySelector('.backdrop').remove();
+    backdropEl.classList.remove('backdrop--show');
     navigationMenuEl.classList.remove('navigation--open');
+};
+
+const handleNavFocusOut = function (e) {
+    const links = this.querySelectorAll('.navigation__link');
+    const lastLink = links[links.length - 1];
+
+    if (e.target === lastLink) {
+        closeNav();
+    }
 };
 
 openMenuBtnEl.addEventListener('click', openNav);
 closeMenuBtnEl.addEventListener('click', closeNav);
+backdropEl.addEventListener('click', closeNav);
+navigationMenuEl.addEventListener('focusout', handleNavFocusOut);
